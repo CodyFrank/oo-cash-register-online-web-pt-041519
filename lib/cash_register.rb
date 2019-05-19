139a2f38b1c0ee60a0fa92f028d9331214543c35
Cody Frank <cody.frank30@gmail.com>
2019-05-19 22:55:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,9 @@ class CashRegister
     if @discount > 0
       deduct_percent = @discount/100.to_f
       @total = @total - @total * deduct_percent
+      "After the discount, the total comes to #{@total}"
+    else
+      ""
     end
-    @total
   end
 end
