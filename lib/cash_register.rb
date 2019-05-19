a9e473f6594f451d4f8b8a8df69fabea2c75796a
Cody Frank <cody.frank30@gmail.com>
2019-05-19 22:10:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,12 +3,12 @@
 class CashRegister
   attr_accessor :total
   def initialize
-    @total = 0 
+    @total = 0
   end
   def cash_register_with_discount(disc)
     self.new
     @total = 20
-      
+
   end
 
 end
