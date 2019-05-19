afde5c8b887c7054ab449755c61eef59eaa50b47
Cody Frank <cody.frank30@gmail.com>
2019-05-19 23:04:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount != nil 
+    if @discount != nil
       deduct_percent = @discount/100.to_f
       @total = @total - @total * deduct_percent
       "After the discount, the total comes to $#{@total.to_i}."
